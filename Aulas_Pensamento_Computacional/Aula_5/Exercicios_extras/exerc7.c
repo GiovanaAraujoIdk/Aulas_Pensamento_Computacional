2ff3df43b748703a33a9ca65830f58d3497809d7
Giovana <giovananunesdasilvar389182@gmail.com>
2026-09-17 16:54:06 -0300

    Arrumando lógica do exer_7

--- Aulas_Pensamento_Computacional/Aula_5/Exercicios_extras/exerc7.c
+++ Aulas_Pensamento_Computacional/Aula_5/Exercicios_extras/exerc7.c
@@ -3,36 +3,25 @@ int main() {
     float valor1, valor2, valor3, valor4;
     
     printf("Digite o primeiro valor: ");
-    scanf("%f", valor1);
+    scanf("%f", &valor1);
 
     printf("Digite o segundo valor: ");
-    scanf("%f", valor2);
+    scanf("%f", &valor2);
 
     printf("Digite o terceiro valor: ");
-    scanf("%f", valor3);
+    scanf("%f", &valor3);
 
     printf("Digite o quarto valor: ");
-    scanf("%f", valor4);
+    scanf("%f", &valor4);
     
-    if (valor1 !=> valor2, valor3, valor4)
-{
-    printf("o menor valor e: %f", valor1);
-}
-    else if (valor2 !=> valor1, valor3, valor4)
-{
-    printf("o menor valor e: %f", valor2);
-}
-    else if (valor3 !=> valor1, valor2, valor4) 
-{
-    printf("o menor valor e: %f", valor3);
-}
-    else if (valor4 !=> valor1, valor2, valor3) 
-{
-    printf("o menor valor e: %f", valor4);
-}
-else {
-
-}
-
+    if (valor1 < valor2 && valor1 < valor3 && valor1 < valor4) {
+        printf("O menor valor e: %.2f", valor1);
+    } else if (valor2 < valor1 && valor2 < valor3 && valor2 < valor4) {
+        printf("O menor valor e: %.2f", valor2);
+    } else if (valor3 < valor1 && valor3 < valor2 && valor3 < valor4) {
+        printf("O menor valor e: %.2f", valor3);
+    } else {
+        printf("O menor valor e: %.2f", valor4);
+    }
 return 0;
 }
